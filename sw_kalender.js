1c7b96747924234c5090fad6636b2e5412d1d57a
vsoechtig-del <v.soechtig@gmail.com>
2026-04-26 13:30:43 +0200

    Add files via upload

---
+++ sw_kalender.js
@@ -0,0 +1,20 @@
+const CACHE = 'reuniao-kalender-v1';
+const FILES = ['reuniao_kalender.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
